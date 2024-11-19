e1641fe863e83bbcb5a2732a5db95a48ad21917d
C0A23009 <c0a23009ae@edu.teu.ac.jp>
2024-11-19 15:55:35 +0900

    背景に動きを追加修正

--- pygame_image.py
+++ pygame_image.py
@@ -12,14 +12,15 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
+    bgf_img = pg.transform.flip(bg_img, True, True)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        screen.blit(bg_img, [-tmr, 0])#scereen surfaceに背景画像surfaceを張り付ける
+        x = tmr % 1600
+        screen.blit(bg_img, [-x, 0])#scereen surfaceに背景画像surfaceを張り付ける
+        screen.blit(bgf_img, [-x + 1600, 0])
         screen.blit(kk_img, [300, 200])
-        if tmr >= 1600 - 800:
-            tmr = 0
         pg.display.update()
         tmr += 1        
         clock.tick(200)
